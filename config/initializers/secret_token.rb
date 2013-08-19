0e1ae6f644b9c892675acc637173ebb742302b01
vagrant <vagrant@precise32.(none)>
2013-08-19 21:18:35 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dayme::Application.config.secret_token = '8da4df1311b55f185380e058596c389c29a5dcf074a9cb37fa1cf5586d84ce837c06ef8f85957acda98c6dd818076e50b6f2ed2a285ec8862294308a5bb50668'
